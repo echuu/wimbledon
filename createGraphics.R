81cfd9daa9b56a8cc828309e87657f1e52abcf33
echuu <chuu3ric@gmail.com>
2017-07-15 10:02:51 -0700

    processed data

---
+++ createGraphics.R
@@ -0,0 +1,19 @@
+
+
+library("dplyr")
+library("reshape2")
+library("ggplot2")
+
+
+# visual 1-- double sided bar plot
+    # side by side comparison of road to final match statistics
+    # double-sided bar plot
+
+# visual 2 -- hour glass visual ~ # of hours
+    # hours spent on court
+    # compare to previous years that he has won
+
+# visual 3 --- line graph
+    # FSPW over the years
+    # RPW over years
+    # BPC over years
